fa365dbdcd61b391dc52a9c6e36a27de44e86b97
Paolo <paolo@proofmedia.io>
2018-11-10 23:32:13 +0100

    Better outputs

--- src/app.py
+++ src/app.py
@@ -5,26 +5,36 @@ import shutil
 import subprocess
 import threading
 
+MAX_LINE_COLS = 79
 NODE_BINARY = 'rnode'
 DATA_DIR = '/tmp/rnode_validator_keys'
 
 
 def output_reader(proc):
+
+    line_number = 0
     for line in iter(proc.stdout.readline, b''):
+        line_number += 1
         line_string = line.decode('utf-8').lower()
-        print('OUT: %s' % line_string, end='')
+        line_stripped = line_string.split('[main]').pop().strip()
+        running_output = 'RUNNING (line %s): %s' % (line_number, line_stripped)
+        print(running_output[:MAX_LINE_COLS] + '\r', end='')
+
         if 'genesis' in line_string:
             if 'created validator' in line_string:
                 proc.terminate()
-                print("FINISHED!")
+                print('.' * MAX_LINE_COLS)
+                print("\nCompleted\n")
 
 
 def proc_wait(proc):
+    seconds = 0
     while True:
-        still_running = proc.poll() is None
-        if still_running:
-            print("Node running:", still_running)
+        seconds += 1
+        if proc.poll() is None:
             time.sleep(1)
+            if seconds % 30 == 0:
+                print("Warning: node still running after %s seconds" % seconds)
         else:
             break
 
@@ -33,9 +43,10 @@ def make_sure_process_is_closed(proc):
     proc.terminate()
     try:
         proc.wait(timeout=0.2)
-        print('Process return code: ', proc.returncode)
+        # print('Process return code: ', proc.returncode)
     except subprocess.TimeoutExpired:
-        print('Process did not terminate in time')
+        pass
+        # print('Process did not terminate in time')
 
 
 def remove_existing_data(dir):
@@ -55,18 +66,23 @@ def run_process(command):
     return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
-def run_all_threads(args):
-    t1 = threading.Thread(target=output_reader, args=args)
-    t2 = threading.Thread(target=proc_wait, args=args)
-    t1.start() or t2.start()
-    t1.join() or t2.join()
+def run_all_threads(functions, args):
+    threads = []
+    for function in functions:
+        t = threading.Thread(target=function, args=args)
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
 
 
 def main():
     remove_existing_data(DATA_DIR)
     proc = run_process(build_command(NODE_BINARY, DATA_DIR))
+    print("RNode launched.\n")
     args = (proc,)
-    run_all_threads(args)
+    functions = [output_reader, proc_wait]
+    run_all_threads(functions, args)
     make_sure_process_is_closed(*args)
 
 
